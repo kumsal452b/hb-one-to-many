20e5507a47b99b1db375641b53b7dabd07009b01
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-02 19:52:27 +0300

    add many to one annotatiion

--- src/com/kumsal/demo/entity/Course.java
+++ src/com/kumsal/demo/entity/Course.java
@@ -1,10 +1,29 @@
 package com.kumsal.demo.entity;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="course")
 public class Course {
-		
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="title")
+	private String title;
+	
+	@ManyToOne(cascade={CascadeType.DETACH, CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+	private Instructor instructor;
+	public Course() {
+		// TODO Auto-generated constructor stub
+	}
+	
 }
