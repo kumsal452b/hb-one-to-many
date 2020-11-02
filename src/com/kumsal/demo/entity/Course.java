13548c431f7a3b1a354ca345c6f536391c806b18
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-02 19:47:25 +0300

    add nea annotation

--- src/com/kumsal/demo/entity/Course.java
+++ src/com/kumsal/demo/entity/Course.java
@@ -1,5 +1,10 @@
 package com.kumsal.demo.entity;
 
-public class Course {
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="course")
+public class Course {
+		
 }
