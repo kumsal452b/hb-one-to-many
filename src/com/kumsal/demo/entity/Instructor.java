655dd430d3442174ad913aafc7a506d45f0c3017
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-02 20:28:23 +0300

    Nothıng

--- src/com/kumsal/demo/entity/Instructor.java
+++ src/com/kumsal/demo/entity/Instructor.java
@@ -37,7 +37,7 @@ public class Instructor {
 	@JoinColumn(name="instructor_detail_id")
 	private InstructorDetail instructorDetail;
 	
-	@OneToMany(cascade={CascadeType.DETACH, CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+	@OneToMany(mappedBy="instructor",cascade={CascadeType.DETACH, CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
 	private List<Course> course;
 	
 	public Instructor() {
